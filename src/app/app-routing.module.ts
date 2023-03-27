60bdde8a93f3f61a4596a9a4137dd1245b0cecb0
Formateur1 <enseignant.formateur@protonmail.com>
2023-03-27 16:04:00 +0200

    fonctionnalites divers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewsComponent } from './news/news.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RechercheComponent } from './recherche/recherche.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about', component: AboutComponent, children: [{path:'news', component: NewsComponent}]},
+  {path:'accueil', component: AccueilComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'recherche', component: RechercheComponent},
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    AccueilComponent,
+    PageNotFoundComponent,
+    RechercheComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
